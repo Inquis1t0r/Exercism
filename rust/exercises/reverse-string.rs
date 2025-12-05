65dc2bbb5fbeab790adfcd188f0027590da2922d
Inquis1t0r <pi0tr3k9292@gmail.com>
2025-12-05 11:47:27 +0100

    Update reverse-string.rs

--- rust/exercises/reverse-string.rs
+++ rust/exercises/reverse-string.rs
@@ -2,5 +2,5 @@ pub fn reverse(input: &str) -> String {
     let char_vec: Vec<char> = input.chars().collect();
     let reverse_result = char_vec.iter().cloned().rev().collect::<String>();
     dbg!(&reverse_result);
-    return reverse_result;
+    reverse_result
 }
